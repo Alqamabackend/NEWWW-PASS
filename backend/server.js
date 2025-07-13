f1a63f7fde5ee51798b464224e9eb37ce7d8dd77
alqama <none>
2025-07-13 13:07:30 +0530

    Your message here

--- backend/server.js
+++ backend/server.js
@@ -8,7 +8,11 @@ connectDB();
 
 const app = express();
 
-app.use(cors());
+app.use(cors({
+  origin: ["http://localhost:5173", "https://newww-pass.vercel.app"],
+  credentials: true,
+}));
+
 app.use(express.json());
 
 // Routes
